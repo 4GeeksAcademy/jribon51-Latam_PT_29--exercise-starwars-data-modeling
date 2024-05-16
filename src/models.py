e6a535b3919ffaab90874f8c37c1badc07b58a7b
jribon51 <151412716+jribon51@users.noreply.github.com>
2024-05-16 05:04:44 +0000

    hace falta el diagrama nada mas externo

--- src/models.py
+++ src/models.py
@@ -25,45 +25,37 @@ class Address(Base):
     post_code = Column(String(250), nullable=False)
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
+    
+class Country(Base):
+    __tablename__='country'
+    id=Column(Integer,primary_key=True)
+    name = Column(String(250))
 
 class User(Base):
     __tablename__='user'
     id=Column(Integer,primary_key=True)
-    username=Column(String(50))
-    firstname=Column(String(50))
-    lastname=Column(String(50))
-    email=Column(String(50))
+    Name=Column(String(50))
+    email=Column(String(50),unique=True)
+    password=Column(String(32))
+    id_country=Column(Integer,ForeignKey('country.id'))
+    country=relationship(Country)
 
-class Post(Base):
-    __tablename__='post'
+class Planets(Base):
+    __tablename__='planets'
     id=Column(Integer,primary_key=True)
-    user_id=Column(Integer,ForeignKey('user.id'))
+    name=Column(String(50))
+    population=Column(Integer)
+
+class FavoritePlanet(Base):
+    __tablename__='favorite_planet'
+    id=Column(Integer,primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
     user=relationship(User)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet=relationship(Planets)
 
-class Comment(Base):
-    __tablename__='comment'
-    id=Column(Integer,primary_key=True)
-    comment_text=Column(String(50))
-    author_id=Column(Integer,ForeignKey('user.id'))
-    user=relationship(User)
-    post_id=Column(Integer,ForeignKey('post.id'))
-    post=relationship(Post)
 
-class Media(Base):
-    __tablename__='media'
-    id=Column(Integer,primary_key=True)
-    type=Column(Enum('image', 'video', name='media_type_enum'))
-    url=Column(String(50))
-    post_id=Column(Integer,ForeignKey('post.id'))
-    post=relationship(Post)
 
-class Follower(Base):
-    __tablename__='follower'
-    id=Column(Integer,primary_key=True)
-    user_from_id=Column(Integer,ForeignKey('user.id'))
-    user = relationship(User)
-    user_to_id=Column(Integer,ForeignKey('user.id'))
-    user_id=relationship('User')
 
 
     def to_dict(self):
